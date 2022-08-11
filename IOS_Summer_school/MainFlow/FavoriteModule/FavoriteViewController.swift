d8b6d5ba09dec768428968b488b8c9fd7eba3aa2
DedKiri11 <alekstim01@gmail.com>
2022-08-11 19:15:37 +0300

    Favorite screen
    favorite screen without logic only on test data

--- IOS_Summer_school/MainFlow/FavoriteModule/FavoriteViewController.swift
+++ IOS_Summer_school/MainFlow/FavoriteModule/FavoriteViewController.swift
@@ -9,20 +9,38 @@ import UIKit
 
 class FavoriteViewController: UIViewController, UIGestureRecognizerDelegate {
 
-    @IBOutlet weak var SearchButton: UIButton!{
-        didSet{
-            let image = UIImage(named: "SearchButton")
-            SearchButton.setImage(image, for: .normal)
-            SearchButton.setTitle("", for: .normal)
-            
-        }
-    }
+    //MARK: - View
     
+    @IBOutlet weak var collectionView: UICollectionView!
+    
+    // MARK: - Private Properties
+    
+    private let model: MainModel = .init()
+    
+//    @IBOutlet weak var SearchButton: UIButton!{
+//        didSet{
+//            let image = UIImage(named: "SearchButton")
+//            SearchButton.setImage(image, for: .normal)
+//            SearchButton.setTitle("", for: .normal)
+//
+//        }
+//    }
+    
+    
+    // MARK: - Lifeсyrcle
     override func viewDidLoad() {
         super.viewDidLoad()
+        configureApperance()
+        configureModel()
+        model.getPosts()
 
     }
-
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        configureNavigationBar()
+    }
+    
+    //MARK: - SearchButton
     @objc func searchButtonTapped(){
         print("searchButtonTappedFavorite")
         let vc = SearchViewController()
@@ -31,10 +49,7 @@ class FavoriteViewController: UIViewController, UIGestureRecognizerDelegate {
         navigationController?.pushViewController(vc, animated: true)
     }
     
-    override func viewWillAppear(_ animated: Bool) {
-        super.viewWillAppear(animated)
-        configureNavigationBar()
-    }
+    
     
     
 
@@ -51,3 +66,60 @@ extension FavoriteViewController {
         navigationController?.interactivePopGestureRecognizer?.delegate = self
     }
 }
+// MARK: - Private Methods
+private extension FavoriteViewController {
+    
+    func configureApperance(){
+        collectionView.register(UINib(nibName: "\(FavoriteTableViewCell.self)", bundle: .main), forCellWithReuseIdentifier: "\(FavoriteTableViewCell.self)")
+        collectionView.dataSource = self
+        collectionView.delegate = self
+        collectionView.contentInset = .init(top: 10, left: 16, bottom: 10, right: 16)
+
+    }
+    
+    func configureModel() {
+        model.didItemsUpdated = { [weak self] in
+            DispatchQueue.main.async {
+                self?.collectionView.reloadData()
+            }
+        }
+    }
+    
+    
+}
+
+// MARK: - UICollection
+extension FavoriteViewController: UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "\(FavoriteTableViewCell.self)", for: indexPath)
+        if let cell = cell as? FavoriteTableViewCell {
+            let item = model.items[indexPath.row]
+            cell.title = item.title
+            cell.isFavorite = item.isFavorite
+            cell.image = item.image
+            cell.date = item.dataCreation
+            cell.text = item.content
+        }
+        return cell
+    }
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return CGSize(width: view.frame.width - 32, height: view.frame.height * 0.48)
+    }
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
+        return 24
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        let vc = DetailViewController()
+        vc.model = model.items[indexPath.row]
+        navigationController?.pushViewController(vc, animated: true)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return model.items.count
+    }
+    
+}
+
+
